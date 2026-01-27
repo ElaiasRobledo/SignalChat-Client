60ef925a2006834ebf44c3ebd5ce9ad9a262b8dc
elias.vulcano.lubricacion@gmail.com <eliasrobledoxx@hotmail.com>
2026-01-27 01:39:27 +0100

    feat: Set and implement Api base url from appsetting.json and link it to HttpClient

--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -10,16 +10,30 @@ using Application.Interfaces.Chat;
 using Infrastructure.Chat;
 using Application.Services;
 using Application.Events;
+using Infrastructure.Utils;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 
 namespace Infrastructure
 {
     public static class DependencyInjection
     {
         public static IServiceCollection AddInfrastructure(
-        this IServiceCollection services)
+        this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddHttpClient<IContacts, ContactsService>();
-            services.AddHttpClient<IAuth, AuthClientService>();
+            services.Configure<ApiOptions>(configuration.GetSection("Api"));
+            services.AddHttpClient<IContacts, ContactsService>((sp, client)
+                =>
+            {
+                var options = sp.GetRequiredService<IOptions<ApiOptions>>().Value;
+                client.BaseAddress = new Uri(options.BaseUrl + "contacts/");
+            });
+
+            services.AddHttpClient<IAuth, AuthClientService>((sp, client) =>
+            {
+                var options = sp.GetRequiredService<IOptions<ApiOptions>>().Value;
+                client.BaseAddress = new Uri(options.BaseUrl + "auth/");
+            });
             services.AddSingleton<ISignalRClient, SignalRClient>();
             services.AddSingleton<ChatService>();
             
