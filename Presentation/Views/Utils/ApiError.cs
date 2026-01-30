f94c57616547b5fcb7bfb0ba90d0df114cd4cb27
elias.vulcano.lubricacion@gmail.com <eliasrobledoxx@hotmail.com>
2026-01-30 02:35:47 +0100

    feat(ApiError): add public class with propery for deserializing message error from server

---
+++ Presentation/Views/Utils/ApiError.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Presentation.Views.Utils
+{
+    public class ApiError
+    {
+        [JsonPropertyName("message")]
+        public string Message { get; set; }
+
+
+    }
+}
---
+++ Presentation/Views/Utils/Helpers.cs
@@ -0,0 +1,20 @@
+﻿using Presentation.Views.Decorations;
+using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Presentation.Views.Utils
+{
+    public class Helpers
+    {
+
+        public void DrawHeader()
+        {
+            AnsiConsole.Clear();
+            Banner.Show();
+            AnsiConsole.WriteLine();
+        }
+
+    }
+}
